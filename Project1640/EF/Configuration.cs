550d4a2ddbdae33b5deca48885cab7b77b06ffa2
Garung69 <thienlmgch190355@fpt.edu.vn>
2022-03-07 15:37:13 +0700

    Create EF code first

---
+++ Project1640/EF/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Project1640.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Project1640.EF.CMSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"EF";
+        }
+
+        protected override void Seed(Project1640.EF.CMSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
