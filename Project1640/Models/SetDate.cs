baf2957f66fe5142147d60e7d863ad68eaac8323
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2022-03-23 00:51:02 +0700

    update database

---
+++ Project1640/Models/SetDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1640.Models
+{
+    public class SetDate
+    {
+        public int Id { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
